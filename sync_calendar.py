2c1f05f579689c6c08a2bb47f6357cec25685afa
Chakradhar Sai Adini <chakradharsai5@gmail.com>
2026-04-01 10:35:01 +0530

    Update sync_calendar.py

--- sync_calendar.py
+++ sync_calendar.py
@@ -84,7 +84,7 @@ while response.get("has_more"):
 for page in results:
     title = "No title"
     if page["properties"]["Thing"]["title"]:
-        title = page["properties"]["Name"]["title"][0]["plain_text"]
+        title = page["properties"]["Thing"]["title"][0]["plain_text"]
         print("Task:", title)
 
     props = page["properties"]
